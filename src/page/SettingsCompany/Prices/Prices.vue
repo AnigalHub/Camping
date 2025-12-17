4bc7e2fc03c767901a4969e95d0e8bb39c9f8e63
AnigalHub <anigal1997@gmail.com>
2025-12-17 14:56:26 +0300

    разделены на блоки цены

--- src/page/SettingsCompany/Prices/Prices.vue
+++ src/page/SettingsCompany/Prices/Prices.vue
@@ -3,30 +3,49 @@
     <v-row dense align="stretch">
       <v-col cols="9">
         <v-card class="pa-6" elevation="2">
-          <v-form>
-            <h3 class="form-subtitle">Цены</h3>
-            <div class="grid-inputs">
-              <v-text-field v-model="prices.person" label="За человека" variant="outlined" density="comfortable"
-                rounded="lg" clearable v-maska="'#########'" />
-              <v-text-field v-model="prices.animal" label="За животное" variant="outlined" density="comfortable"
-                rounded="lg" clearable v-maska="'#########'" />
-              <v-text-field v-model="prices.tent" label="За аренду палатки (Стандарт 2+1)" variant="outlined" density="comfortable"
-                rounded="lg" clearable v-maska="'#########'" />
-              <v-text-field v-model="prices.familyTent" label="За аренду палатки (Семейная 2+1)" variant="outlined" density="comfortable"
-                rounded="lg" clearable v-maska="'#########'" />
-              <v-text-field v-model="prices.premiumTent" label="За аренду палатки (Премиум тент-хаус)" variant="outlined" density="comfortable"
-                rounded="lg" clearable v-maska="'#########'" />
-              <v-text-field v-model="prices.transport" label="За транспорт" variant="outlined" density="comfortable"
-                rounded="lg" clearable v-maska="'#########'" />
-              <v-text-field v-model="prices.smallTransport" label="За мелкий транспорт" variant="outlined" density="comfortable" 
-              rounded="lg" clearable v-maska="'#########'" />
+          <v-form class="form-layout">
+            <!-- Цены -->
+            <div class="section-block">
+              <h3 class="form-subtitle">Цены</h3>
+              <div class="grid-inputs">
+                <v-text-field v-model="prices.person" label="За человека" variant="outlined" density="comfortable"
+                  rounded="lg" clearable v-maska="'#########'" type="number"/>
+                <v-text-field v-model="prices.animal" label="За животное" variant="outlined" density="comfortable"
+                  rounded="lg" clearable v-maska="'#########'" type="number"/>
+              </div>
             </div>
-            <v-btn class="btn-page" :disabled="!isChanged" :class="{ 'btn-disabled': !isChanged }">Сохранить</v-btn>
+            <!-- Транспорт -->
+            <div class="section-block">
+              <h3 class="form-subtitle">Транспорт</h3>
+              <div class="grid-inputs">
+                <v-text-field v-model="prices.transport" label="Машина/автодом" variant="outlined" density="comfortable"
+                  rounded="lg" clearable v-maska="'#########'" type="number"/>
+                <v-text-field v-model="prices.smallTransport" label="Мотоцикл" variant="outlined" density="comfortable"
+                  rounded="lg" clearable v-maska="'#########'" type="number"/>
+              </div>
+            </div>
+            <!-- Глэмпинг -->
+            <div class="section-block">
+              <h3 class="form-subtitle">Глэмпинг</h3>
+              <div class="grid-inputs">
+                <v-text-field v-model="prices.tent" label="Стандарт 2+1" variant="outlined" density="comfortable"
+                  rounded="lg" clearable v-maska="'#########'" type="number"/>
+                <v-text-field v-model="prices.familyTent" label="Семейная 2+1" variant="outlined" density="comfortable"
+                  rounded="lg" clearable v-maska="'#########'" type="number"/>
+                <v-text-field v-model="prices.premiumTent" label="Премиум тент-хаус" variant="outlined"
+                  density="comfortable" rounded="lg" clearable v-maska="'#########'" type="number"/>
+              </div>
+            </div>
+            <div class="actions">
+              <v-btn class="btn-page" :disabled="!isChanged" type="submit">
+                Сохранить
+              </v-btn>
+            </div>
           </v-form>
         </v-card>
       </v-col>
       <v-col class="content-icons">
-        <icon-circle :svg="CacheSvg" text="Действующие тарифы" :min="true"/>
+        <icon-circle :svg="CacheSvg" text="Действующие тарифы" :min="true" :animated="true"/>
       </v-col>
     </v-row>
   </div>
@@ -59,10 +78,54 @@ const isChanged = computed(() => {
 });
 </script>
 
+<style>
+  .section-block  .v-field {
+  background: #ffffff8a;
+}
+</style>
 <style scoped>
 @import "../../../../public/form.css";
+
 .content {
   padding: 20px 10px;
   height: 70vh;
+}
+
+/* Общий layout формы */
+.form-layout {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+}
+
+/* Визуальные секции */
+.section-block {
+  background: #f6f7f8;
+  border-radius: 14px;
+  padding: 8px 10px 0;
+  border: 1.5px solid rgba(70, 120, 170, .35);
+}
+.section-block:hover {
+  border-color: #4a90e2;
+}
+
+/* Сетка инпутов */
+.grid-inputs {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 8px;
+  margin-top: 8px;
+}
+
+.actions {
+  display: flex;
+  justify-content: flex-start;
+  gap: 16px;
+  margin-top: 10px;
+}
+
+.btn-page {
+  width: auto;
+  margin: -10px 0;
 }
 </style>
